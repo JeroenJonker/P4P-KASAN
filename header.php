f4950de56a01a027fa278f8f7f7707d97d7e3249
Stefan Visser <stefan.visser@hotmail.com>
2016-05-24 11:03:42 +0200

    Footer/header

--- header.php
+++ header.php
@@ -1,4 +1,14 @@
 <!DOCTYPE html>
-<html>
-    <head></head>
-    <body>
+<html <?php language_attributes(); ?>>
+    <head>
+		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta name="viewport" content="width=device-width">
+		<title><?php bloginfo('name');?>
+		<?php wp_head(); ?>
+	</head> 
+	
+<body <?php body_class();?>>
+	<header class="site-header">
+		<h1><?php bloginfo('name'); ?></h1>
+		<h5><?php bloginfo('description');?></h5>
+	</header> 
