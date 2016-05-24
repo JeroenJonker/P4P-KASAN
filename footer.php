7aa9a4b57386bc2068b1a0ab4862ae5d94b3aa2a
Stefan Visser <stefan.visser@hotmail.com>
2016-05-24 11:11:36 +0200

    Hello footer

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-        <footer>
-        </footer>
-
+      <footer class="site-footer">
+      	<p><?php bloginfo('name'); ?> -&copy; <?php echo date ('Y')''?></p>
+      	
+      </footer> 
+      <?php wp_footer(); ?>
     </body>
 </html>
