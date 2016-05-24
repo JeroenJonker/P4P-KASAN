52fd656d34f5235cdc659ac6f5c1bd5a1e0f95dc
Jeroen Jonker <jeroenojonker@hotmail.nl>
2016-05-24 13:12:18 +0200

    index opgedeelt in 5 subjecten

--- index.php
+++ index.php
@@ -3,13 +3,49 @@
 
 get_header();
 if(have_posts()) : 
-    while(have_posts()) : the_post(); ?>    
-    <article>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-    </article>
-
-    <?php endwhile;
+    (int)$counter = 0; 
+    while(have_posts() && $counter < 5) : the_post();
+    {
+        if ($counter == 0)
+        { ?>
+            <article>
+                <p>test</p>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article> <?php
+        }
+        elseif ($counter == 1)
+        {?>
+            <article>
+                <p> GEEN IDEE</p>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article> <?php
+        }
+        elseif ($counter == 2)
+        {?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article> <?php
+        }
+        elseif ($counter == 3)
+        {?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article> <?php
+        }
+        elseif ($counter == 4)
+        {?>
+            <article>
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+            </article> <?php
+        }
+                $counter++;
+    }
+    endwhile;
     else : 
         echo '<p> No content </p>';
     endif;
