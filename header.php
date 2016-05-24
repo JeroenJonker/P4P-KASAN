25afa8b6057b00070c4fc98d65bb88d313f67f9f
Jeroen Jonker <jeroenojonker@hotmail.nl>
2016-05-24 11:48:15 +0200

    fixed stuff header

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width">
 		<title><?php bloginfo('name');?>
-		<?php wp_head(); ?>
+        </title>
+        <?php wp_head(); ?>
 	</head> 
 	
 <body <?php body_class();?>>
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <?php
 
 get_header();
-
 if(have_posts()) : 
     while(have_posts()) : the_post(); ?>    
     <article>
