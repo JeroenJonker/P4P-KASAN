329ba33a0feb6f314409a1329cae1e1e03e84dc6
johannes <johannies7@gmail.com>
2016-05-31 11:07:44 +0200

    Trying to merge this version with Push Thing

--- index.php
+++ index.php
@@ -3,52 +3,12 @@
 get_header();
 if(have_posts()) : 
     (int)$counter = 0; 
-    while(have_posts() && $counter < 5) : the_post();
+    while(have_posts() && $counter < 1) : the_post();
     {
-        if ($counter == 0)
-        { ?>
-            <article class="first-post">
-                <?php if (has_post_thumbnail( $post->ID ) ): ?>
-                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-                <img class="testzor" src="<?php echo $image[0]; ?>"/>
-                <!--div id="custom-bg" style="background-image: url('<?php echo $image[0]; ?>')">
+        the_content();
+        //get_template_part('page', get_post_format());
 
-                </div-->
-                <?php endif; ?>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article> <?php
-        }
-        elseif ($counter == 1)
-        {?>
-            <article>
-                <p> GEEN IDEE</p>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article> <?php
-        }
-        elseif ($counter == 2)
-        {?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article> <?php
-        }
-        elseif ($counter == 3)
-        {?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article> <?php
-        }
-        elseif ($counter == 4)
-        {?>
-            <article>
-                <h2><?php the_title(); ?></h2>
-                <?php the_content(); ?>
-            </article> <?php
-        }
-                $counter++;
+        $counter++;
     }
     endwhile;
     else : 
---
+++ page-video.php
@@ -0,0 +1,3 @@
+<article class"post post-video">
+	<?php the_content(); ?>
+</article>
